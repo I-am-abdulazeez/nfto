2e2ee6252bb11a8b003c8262510cb043615d6275
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-06 17:26:26 +0100

    feat: new component SubInfo

---
+++ components/SubInfo.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const SubInfo: React.FC = () => {
+  return (
+    <View>
+      <Text>SubInfo</Text>
+    </View>
+  );
+};
+
+export default SubInfo;
