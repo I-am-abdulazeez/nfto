acd339e31270fdcc791725de4545fc6a490b66eb
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-06 17:05:05 +0100

    chore: imported corresponding prop from interface

--- components/CircleButton.tsx
+++ components/CircleButton.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Text } from "react-native";
+import { CircleButtonProps } from "../interfaces";
 
-const CircleButton: React.FC = () => {
+const CircleButton: React.FC<CircleButtonProps> = () => {
   return (
     <div>
       <Text>imageUrl</Text>
