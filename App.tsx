90ebb6b26c57d09503f9ef12edb2f9d3f6fdada9
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-06 15:56:02 +0100

    feat: Added font sty;es

--- App.tsx
+++ App.tsx
@@ -21,7 +21,17 @@ const theme = {
   },
 };
 
-const App = () => {
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!fontsLoaded) null;
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
