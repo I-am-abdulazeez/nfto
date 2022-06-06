ab7764a8b5c82cce40a4904e5d82e03d14827498
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-06 17:04:17 +0100

    chore: created circleButton component for card

---
+++ components/CircleButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+const CircleButton: React.FC = () => {
+  return (
+    <div>
+      <Text>imageUrl</Text>
+    </div>
+  );
+};
+
+export default CircleButton;
