c626569698b2077f20e8963320afe55295366696
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-06 15:57:41 +0100

    chore: New: HomeHeader Component

---
+++ components/HomeHeader.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const HomeHeader: React.FC = () => {
+  return (
+    <View>
+      <Text> HomeHeader</Text>
+    </View>
+  )
+}
+
+export default HomeHeader
