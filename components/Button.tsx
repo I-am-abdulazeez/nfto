2995ca1f1b5ae6aeab90b01cc421f876c9f800cc
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-06 17:08:22 +0100

    chore: renamed to Button Component to add more exports of different types of Buttons

---
+++ components/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { CircleButtonProps } from "../interfaces";
+
+const CircleButton: React.FC<CircleButtonProps> = () => {
+  return (
+    <div>
+      <Text>imageUrl</Text>
+    </div>
+  );
+};
+
+export { CircleButton };
