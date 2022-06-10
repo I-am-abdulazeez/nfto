2abc4b2bd080ac8f4512ce695d749e2c3513971b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-10 15:52:39 +0100

    fix: remove white space from font-name

--- constants/theme.ts
+++ constants/theme.ts
@@ -18,7 +18,7 @@ export const SIZES = {
 export const FONTS = {
   bold: "Inter_700Bold",
   semiBold: "Inter_600SemiBold",
-  medium: " Inter_500Medium",
+  medium: "Inter_500Medium",
   regular: "Inter_400Regular",
   light: "Inter_300Light",
 };
