0ee1d3584ef308e10331771549d58c6c15604690
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-10 17:57:48 +0100

    chore: exported new components from index file in the components folder

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,16 @@
 import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+
 import { CircleButton } from "./Button";
 
-export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton };
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  DetailsDesc,
+  DetailsBid,
+  CircleButton,
+};
