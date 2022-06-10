7101b1360f4a3195ad91437a1722200e43d6e225
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-10 16:59:20 +0100

    chore/styles: home-header structure with styles

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TextInput } from "react-native";
+import { COLORS, assets, FONTS, SIZES } from "../constants";
 
 const HomeHeader: React.FC = () => {
   return (
-    <View>
-      <Text> HomeHeader</Text>
+    <View style={{ backgroundColor: COLORS.primary, padding: SIZES.font }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Image source={assets.logo} style={{ width: 90, height: 25 }} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default HomeHeader
+export default HomeHeader;
