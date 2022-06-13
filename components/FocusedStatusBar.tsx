e2b292374a787f690ad1cdbb0609e9cbfe184b8d
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-13 16:00:43 +0100

    chore: merged StatusBarProps & FocusStatusBarProps to get full props on FocusStatusBar component

--- components/FocusedStatusBar.tsx
+++ components/FocusedStatusBar.tsx
@@ -1,12 +1,17 @@
-import { StatusBar } from "react-native";
+import { StatusBar, StatusBarProps } from "react-native";
 
 import { useIsFocused } from "@react-navigation/core";
 
 import { FocusedStatusBarProps } from "../interfaces";
 
-const FocusedStatusBar: React.FC<FocusedStatusBarProps> = (props) => {
+const FocusedStatusBar: React.FC<FocusedStatusBarProps & StatusBarProps> = ({
+  backgroundColor,
+  ...rest
+}) => {
   const isFocused = useIsFocused();
-  return isFocused ? <StatusBar animated={true} {...props} /> : null;
+  return isFocused ? (
+    <StatusBar animated={true} backgroundColor={backgroundColor} {...rest} />
+  ) : null;
 };
 
 export default FocusedStatusBar;
