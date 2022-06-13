13331026a523f882fb89382a775d6e52d3aa38a6
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-13 18:18:39 +0100

    refactor: components refactor and removal of unused imports

--- components/index.ts
+++ components/index.ts
@@ -4,7 +4,15 @@ import NFTCard from "./NFTCard";
 import DetailsBid from "./DetailsBid";
 import DetailsDesc from "./DetailsDesc";
 
-import { CircleButton } from "./Button";
+import {
+  EndDate,
+  EthPrice,
+  ImageCmp,
+  NFTTitle,
+  People,
+  SubInfo,
+} from "./SubInfo";
+import { CircleButton, CustRectButton } from "./Button";
 
 export {
   HomeHeader,
@@ -13,4 +21,11 @@ export {
   DetailsDesc,
   DetailsBid,
   CircleButton,
+  CustRectButton,
+  ImageCmp,
+  NFTTitle,
+  People,
+  SubInfo,
+  EndDate,
+  EthPrice,
 };
