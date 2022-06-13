703bc8ba39dbc8d533237e6d3b5bfb078b41ecf9
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-13 17:24:19 +0100

    chore: added NFTTitle and EthPrice component respectively then displayed data

--- components/DetailsDesc.tsx
+++ components/DetailsDesc.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import { View, Text } from "react-native";
+
+import { EthPrice, NFTTitle } from "./SubInfo";
+
+import { COLORS, SIZES, FONTS } from "../constants";
+
 import { DetailsDescProps } from "../interfaces";
 
 const DetailsDesc: React.FC<DetailsDescProps> = ({ data }) => {
   return (
-    <View>
-      <Text>DetailsDesc</Text>
-    </View>
+    <>
+      <View>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.extraLarge}
+          subTitleSize={SIZES.font}
+        />
+        <EthPrice price={data.price} />
+      </View>
+    </>
   );
 };
 
