5d7e1a3018c62822726ae068c80aaf45c707e779
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-13 17:42:05 +0100

    style/chore: current bid styles

--- screens/Details.tsx
+++ screens/Details.tsx
@@ -84,6 +84,17 @@ const Details: React.FC<DetailsProps> = ({ route, navigation }) => {
             <SubInfo />
             <View style={{ padding: SIZES.font }}>
               <DetailsDesc data={data} />
+              {data.bids.length > 0 && (
+                <Text
+                  style={{
+                    fontSize: SIZES.font,
+                    fontFamily: FONTS.semiBold,
+                    color: COLORS.primary,
+                  }}
+                >
+                  Current Bid
+                </Text>
+              )}
             </View>
           </>
         }
