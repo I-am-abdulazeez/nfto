d540b375692fb8bb914fe645c9fedf2383a499a0
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-13 16:12:33 +0100

    chore: importation of DetailsBidprops

--- components/DetailsBid.tsx
+++ components/DetailsBid.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
+import { DetailsBidProps } from "../interfaces";
 
-const DetailsBid = () => {
+const DetailsBid: React.FC<DetailsBidProps> = ({ bid }) => {
   return (
     <View>
       <Text>DetailsBid</Text>
