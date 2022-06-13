98579385299366a6f6a0ec4544e6e5027977ab03
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-13 16:02:15 +0100

    chore: NFTDataType created for type references

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,29 @@
 import { StackScreenProps } from "@react-navigation/stack";
+import { ImageSourcePropType } from "react-native";
+import { NFTData } from "../constants";
+
+export type NFTDataType = {
+  id: string;
+  name: string;
+  creator: string;
+  price: number;
+  description: string;
+  image: ImageSourcePropType;
+  bids: {
+    id: string;
+    name: string;
+    price: number;
+    image: ImageSourcePropType;
+    date: string;
+  }[];
+};
 
 export type StackParams = {
   Home: undefined;
   Details: {
     route: undefined;
     navgation: undefined;
+    data: NFTDataType;
   };
 };
 
