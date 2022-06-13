026355fe26db8d75c97ead9b3ba4942b4c35a874
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-13 16:11:45 +0100

    chore: utilize focusStatusBar component, added FlastList component to display each bid details + css styles

--- screens/Details.tsx
+++ screens/Details.tsx
@@ -1,7 +1,58 @@
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  FlatList,
+} from "react-native";
+import { DetailsBid, FocusedStatusBar } from "../components";
+import { CustRectButton } from "../components/Button";
 
-const Details: React.FC = () => {
-  return <Text>Details</Text>;
+import { FONTS, COLORS, SIZES, assets, SHADOWS } from "../constants";
+
+import { DetailsProps } from "../types";
+
+const Details: React.FC<DetailsProps> = ({ route, navigation }) => {
+  const { data } = route.params;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          zIndex: 1,
+        }}
+      >
+        <CustRectButton
+          minWidth={170}
+          fontSize={SIZES.large}
+          {...SHADOWS.dark}
+        />
+      </View>
+
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: SIZES.extraLarge * 3,
+        }}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default Details;
